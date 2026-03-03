5ea3981b636ab71802b9f36debfda198e1d52bd5
Raymond Augé <rayauge@doublebite.com>
2026-03-03 11:58:39 -0500

    update rbac
    Signed-off-by: Raymond Augé <rayauge@doublebite.com>

--- internal/controller/rbac.go
+++ internal/controller/rbac.go
@@ -5,7 +5,7 @@ package controller
 // +kubebuilder:rbac:groups=batch,resources=jobs,                                       verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=configmaps,                                  verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=pods,                                        verbs=get;list;watch
-// +kubebuilder:rbac:groups=core,resources=secrets,                                     verbs=get;list;watch
+// +kubebuilder:rbac:groups=core,resources=secrets,                                     verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=services,                                    verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=serviceaccounts,                             verbs=get;list;watch
 // +kubebuilder:rbac:groups=gateway.networking.k8s.io,resources=httproutes,             verbs=get;list;watch;create;update;patch;delete
